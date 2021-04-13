22395c991d5d81cc5b4ee4862ddfb6ac16917677
willhollyer <70217134+willhollyer@users.noreply.github.com>
2021-04-13 12:47:15 -0600

    Add files via upload
    This challenge has been adapted from The Turing School’s and Jump Start Lab’s Event Manager and updated to make use of GoogleCivic API. For the first task, I was to determine the government representatives for each attendee based on their zip code (as provided by the CSV file). For each attendee, we were then to include a customized letter that thanks them for attending and provides a list of their representatives (each attendee letter can be found in the output folder).

---
+++ lib/event_manager.rb
@@ -0,0 +1,71 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = '' # Google API Key has been hidden.
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, "w") do |file|
+    file.puts form_letter
+  end
+end
+
+def clean_phone_numbers(phone_number)
+  if phone_number.length == 10
+    phone_number
+  elsif phone_number.length == 11 && phone_number[0] == "1"
+    phone_number[1..10]
+  else 
+    "Incorrect Phone number on file."
+  end
+end
+
+puts "EventManager Initialized!"
+
+contents = CSV.open(
+  "event_attendees.csv",
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+contents.each do |row| 
+  id = row[0]
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id,form_letter)
+end
+
+
+
+
+
